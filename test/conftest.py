aad7a0cc951b6832eaebb6a159d8ee6dd7f0a54e
ChristianWBrock <christian.brock AT posteo.net>
2024-12-02 20:41:29 +0100

    TESTONLY: add pytest_generate_tests for filenames
    Parameterize test using the pytest_generate_tests to use filename everywhere.

---
+++ test/conftest.py
@@ -0,0 +1,54 @@
+import os
+
+import pytest
+
+
+def _data_dir():
+    target = 'data'
+
+    here = os.path.dirname(__file__)
+
+    while True:
+        join = os.path.join(here, target)
+        parent, _ = os.path.split(here)
+
+        if os.path.exists(join):
+            return join
+
+        elif parent and parent != here:
+            here = parent
+
+        else:
+            return None
+
+
+def _get_raw_filenames():
+    datadir = _data_dir()
+
+    filenames = [
+        'betori.CR2',
+        'alpori.CR2',
+        'alpcma.NEF'
+    ]
+
+    return [os.path.join(datadir, fn) for fn in filenames]
+
+
+def _get_fits_filenames():
+    datadir = _data_dir()
+
+    filenames = [
+        'alpleo.FIT',
+        'gamOri_02.fit',
+        'alpOri_01.fit',
+    ]
+
+    return [os.path.join(datadir, fn) for fn in filenames]
+
+
+# Implement the pytest_generate_tests hook
+def pytest_generate_tests(metafunc):
+    if 'raw_filename' in metafunc.fixturenames:
+        metafunc.parametrize("raw_filename", _get_raw_filenames())
+    elif 'fits_filename' in metafunc.fixturenames:
+        metafunc.parametrize("fits_filename", _get_fits_filenames())
--- test/test_plotting.py
+++ test/test_plotting.py
@@ -5,48 +5,20 @@ import pytest
 from bayer.scripts import display_histogram, display_spectrum, visualize_segmentation
 
 
-def _get_raw_filenames():
+def test_display_raw_histogram(raw_filename):
 
-    datadir = _find('data')
+    if not os.path.exists(raw_filename):
+        pytest.skip()
 
-    filenames = [
-        'betori.CR2',
-        'alpori.CR2',
-        'alpcma.NEF'
-    ]
-
-    return [os.path.join(datadir, fn) for fn in filenames]
+    sys.argv = ['dummy', raw_filename]
+    display_histogram.main_raw()
 
 
-def _get_fits_filenames():
-
-    datadir = _find('data')
-
-    filenames = [
-        'alpleo.FIT',
-        'gamOri_02.fit',
-        'alpOri_01.fit',
-    ]
-
-    return [os.path.join(datadir, fn) for fn in filenames]
-
-
-def test_display_raw_histogram():
-
-    for filename in _get_raw_filenames():
-        if not os.path.exists(filename):
-            continue
-        sys.argv = ['dummy', filename]
-        display_histogram.main_raw()
-
-
-def test_display_fits_histogram():
-
-    for filename in _get_fits_filenames():
-        if not os.path.exists(filename):
-            continue
-        sys.argv = ['dummy', filename]
-        display_histogram.main_fits()
+def test_display_fits_histogram(fits_filename):
+    if not os.path.exists(fits_filename):
+        pytest.skip()
+    sys.argv = ['dummy', fits_filename]
+    display_histogram.main_fits()
 
 
 def test_help_histogram():
@@ -56,22 +28,20 @@ def test_help_histogram():
         display_histogram.main_raw()
 
 
-def test_display_raw_spectrum():
+def test_display_raw_spectrum(raw_filename):
 
-    for filename in _get_raw_filenames():
-        if not os.path.exists(filename):
-            continue
-        sys.argv = ['dummy', filename]
-        display_spectrum.main_raw()
+    if not os.path.exists(raw_filename):
+        pytest.skip()
+    sys.argv = ['dummy', raw_filename]
+    display_spectrum.main_raw()
 
 
-def test_display_fits_spectrum():
+def test_display_fits_spectrum(fits_filename):
 
-    for filename in _get_fits_filenames():
-        if not os.path.exists(filename):
-            continue
-        sys.argv = ['dummy', filename]
-        display_spectrum.main_fits()
+    if not os.path.exists(fits_filename):
+        pytest.skip()
+    sys.argv = ['dummy', fits_filename]
+    display_spectrum.main_fits()
 
 
 def test_help_spectrum():
@@ -81,22 +51,20 @@ def test_help_spectrum():
         display_spectrum.main_raw()
 
 
-def test_display_raw_contour():
+def test_display_raw_contour(raw_filename):
 
-    for filename in _get_raw_filenames():
-        if not os.path.exists(filename):
-            continue
-        sys.argv = ['dummy', filename]
-        visualize_segmentation.main_raw()
+    if not os.path.exists(raw_filename):
+        pytest.skip()
+    sys.argv = ['dummy', raw_filename]
+    visualize_segmentation.main_raw()
 
 
-def test_display_fits_contour():
+def test_display_fits_contour(fits_filename):
 
-    for filename in _get_fits_filenames():
-        if not os.path.exists(filename):
-            continue
-        sys.argv = ['dummy', filename]
-        visualize_segmentation.main_fits()
+    if not os.path.exists(fits_filename):
+        pytest.skip()
+    sys.argv = ['dummy', fits_filename]
+    visualize_segmentation.main_fits()
 
 
 def test_help_contour():
@@ -104,21 +72,3 @@ def test_help_contour():
     with pytest.raises(SystemExit, match='0'):
         sys.argv = ['dummy', '--help']
         visualize_segmentation.main_raw()
-
-
-def _find(target):
-
-    here = os.path.dirname(__file__)
-
-    while True:
-        join = os.path.join(here, target)
-        parent, _ = os.path.split(here)
-
-        if os.path.exists(join):
-            return join
-
-        elif parent and parent != here:
-            here = parent
-
-        else:
-            return None
